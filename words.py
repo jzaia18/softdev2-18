573cddefb7abca866b62eced7ec43d664300d40f
Jake Zaia <jzaia@stuy.edu>
2018-04-30 16:09:32 -0400

    Smol updates

--- words.py
+++ words.py
@@ -1,13 +1,18 @@
 f = open('book.txt','r')
-book = f.read()
+book = f.read().split()
 f.close()
 
 def frequency(word):
-	return len(filter(lambda w: w.lower() in word.lower(),book.split()))
+  return len(filter(lambda w: w.lower() in word.lower(), book))
 
 def total_frequency(word_list):
-	return sum(map(frequency,word_list))
+  return sum(map(frequency,word_list))
 
-def most_used(word_list):
-	f = map(frequency,word_list)
-	return reduce(lambda a,b: a if a > b else b,word_list)
+def most_used():
+  f = map(frequency, book)
+  return reduce(lambda a, b: a if a > b else b, f)
+
+print frequency("juliet")
+print frequency("romeo")
+print total_frequency(["romeo", "juliet"])
+print most_used()
